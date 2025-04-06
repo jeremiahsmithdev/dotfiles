af0fcd15cdf4d3fc2872f85601030849a617a07f
Jeremiah Smith <jeremiah@jeremiahsmith.dev>
2025-04-06 12:01:38 +1000

    add wezterm.lua

---
+++ wezterm.lua
@@ -0,0 +1,19 @@
+return {
+   hide_tab_bar_if_only_one_tab = true,
+
+	color_scheme = "gruvbox_material_dark_hard",
+    color_schemes = {
+        ["gruvbox_material_dark_hard"] = {
+            foreground = "#D4BE98",
+            background = "#1D2021",
+            cursor_bg = "#D4BE98",
+            cursor_border = "#D4BE98",
+            cursor_fg = "#1D2021",
+            selection_bg = "#D4BE98" ,
+            selection_fg = "#3C3836",
+
+            ansi = {"#1d2021","#ea6962","#a9b665","#d8a657", "#7daea3","#d3869b", "#89b482","#d4be98"},
+            brights = {"#eddeb5","#ea6962","#a9b665","#d8a657", "#7daea3","#d3869b", "#89b482","#d4be98"},
+        },
+    },
+}
